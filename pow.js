de087cde98c30e76fa46057494117edec3a90774
Arkadiy <5672735@gmail.com>
2016-09-20 16:21:10 +0300

    Update pow.js

--- pow.js
+++ pow.js
@@ -4,3 +4,15 @@ var n = +prompt ('В какую степень');
 console.log ("n=", n)
 
 function pow(a,n) {
+  
+var result = 1;
+
+for (var i=0; i<n; i++){
+	result=result * a;
+}
+
+	return result;
+}
+	var powResult = pow(x,n);
+	
+	console.log(powResult);
